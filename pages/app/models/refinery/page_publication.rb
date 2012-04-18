f3b7d02ed1abda98209eeffa60ec6b82e17fa6e1
Nicholas Schultz-Møller <nicholas@subsis.com>
2012-04-18 15:26:07 +0200

    Fixes typo and adds validations.

--- pages/app/models/refinery/page_publication.rb
+++ pages/app/models/refinery/page_publication.rb
@@ -2,8 +2,12 @@
 
 module Refinery
   class PagePublication < Refinery::Core::BaseModel
-    belongs_to :page, :foreign_key => :refinery_page_pid
+    belongs_to :page, :foreign_key => :refinery_page_id
 
     attr_accessible :publish_at, :locale
+
+    validates :publish_at,
+              :locale,
+              :presence => true
   end
 end
